37147589c1a6b237dc1fefbb2df517bfed5758c1
AloWarrior2000 <39768809+AloWarrior2000@users.noreply.github.com>
2019-09-27 17:56:19 -0500

    Iterator changes

--- iterator.h
+++ iterator.h
@@ -1,40 +1,63 @@
 #ifndef ITERATOR_H
 #define ITERATOR_H
 
+#include <stack>
 #include "node.h"
 
 template <typename T> 
 class Iterator {
     private:
         Node<T> *current;
+        stack<Node<T>*> pp;
 
     public:
         Iterator() {
-            // TODO
+            current = nullptr;
         }
 
-        Iterator(Node<T> *node) {
-            // TODO
+        explicit Iterator(Node<T> *node) {
+            current = node;
         }
 
-        Iterator<T>& operator=(const Iterator<T> &other) {          
-            // TODO
+        Iterator<T>& operator=(const Iterator<T> &other) {
+            current = Iterator<T>(other.current);
+            return *this;
         }
 
         bool operator!=(Iterator<T> other) {
-            // TODO
+            return current->data != other.current->data;
+        }
+
+        void setstackpp(Node<T>* theRoot){
+            pp.push(theRoot);
+            while(theRoot->left){
+                theRoot = theRoot->left;
+                pp.push(theRoot);
+            }
+            pp.pop();
         }
 
         Iterator<T>& operator++() {
-            // TODO
+            auto* aux = current;
+            if (aux->right){
+                aux = aux->right;
+                pp.push(aux);
+                while (aux->left){
+                    aux = aux->left;
+                    pp.push(aux);
+                }
+            }
+            current = pp.top();
+            pp.pop();
+            return *this;
         }
 
         Iterator<T>& operator--() {
-            // TODO
+
         }
 
         T operator*() {
-            // TODO
+            return current->data;
         }
 };
 
